3746cd43e3053914121aa37cf4df05347286c9f4
agui-coder <894784649@qq.com>
2023-12-17 19:05:30 +0800

    types: upgrade pay.yaml

--- pay.go
+++ pay.go
@@ -38,7 +38,7 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "etc/pay_dev.yaml", "the config file")
+var configFile = flag.String("f", "etc/pay.yaml", "the config file")
 
 func main() {
 	flag.Parse()
